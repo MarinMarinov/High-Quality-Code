a4796eb8863c2f25fa4a0cfb83e91e1583c471e2
MarinMarinov <marin.telerik@gmail.com>
2015-07-17 13:24:30 +0300

    Initial commitment

---
+++ HW04CorrectUseOfVariablesDataExpressionsAndConstants/01.ClassSize/Figure.cs
@@ -0,0 +1,68 @@
+﻿namespace ClassSizeInCSharp
+{
+    using System;
+
+    public class Figure
+    {
+        private double width;
+        private double height;
+
+        public Figure(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Width value must be positive number");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Height value must be positive number");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public static Figure GetRotatedFigure(Figure figure, double angleOfRotation)
+        {
+            double sinOfAngle = Math.Sin(angleOfRotation);
+            double cosOfAngle = Math.Cos(angleOfRotation);
+
+            double modulOfSinus = Math.Abs(sinOfAngle);
+            double modulOfCosinus = Math.Abs(cosOfAngle);
+
+            double rotatedFigureWidth = (modulOfCosinus * figure.width) + (modulOfSinus * figure.height);
+            double rotatedFigureHeight = (modulOfSinus * figure.width) + (modulOfCosinus * figure.height);
+
+            Figure rotatedFigure = new Figure(rotatedFigureWidth, rotatedFigureHeight);
+
+            return rotatedFigure;            
+        }
+    }
+}
---
+++ HW04CorrectUseOfVariablesDataExpressionsAndConstants/02.MethodPrintStatistics/Print.cs
@@ -0,0 +1,43 @@
+﻿namespace MethodPrintStatistics
+{
+    using System;
+    using System.Linq;
+
+    public class Print
+    {
+        public static void Main(string[] args)
+        {
+            double[] numbers = new double[] { 1.2, 2.4, 3.6, 4.5, 5.6 };
+
+            PrintStatistics(numbers);
+        }
+
+        public static void PrintStatistics(double[] numbers)
+        {
+            Console.WriteLine(MessageTemplates.MaximumNumberMessage, FindMaxNumber(numbers));
+            Console.WriteLine(MessageTemplates.MinimumNumberMessage, FindMinNumber(numbers));
+            Console.WriteLine(MessageTemplates.AverageNumberMessage, FindAverageNumber(numbers));
+        }
+
+        private static double FindMaxNumber(double[] numbers)
+        {
+            double maxNumber = numbers.Max();
+
+            return maxNumber;
+        }
+
+        private static double FindMinNumber(double[] numbers)
+        {
+            double minNumber = numbers.Min();
+
+            return minNumber;
+        }
+
+        private static double FindAverageNumber(double[] numbers)
+        {
+            double averageNumber = numbers.Average();
+
+            return averageNumber;
+        }
+    }
+}
