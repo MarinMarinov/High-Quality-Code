4c0103844433dd22cf076611fb988151c8a8a078
Marin Marinov <marin.telerik@gmail.com>
2015-10-14 21:09:04 +0300

    HW11 added

---
+++ HW11TestDrivenDevelopment/Homework/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Poker
+{
+    public class Hand : IHand
+    {
+        public IList<ICard> Cards { get; private set; }
+
+        public Hand(IList<ICard> cards)
+        {
+            this.Cards = cards;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.Cards);
+        }
+    }
+}
